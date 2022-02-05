a00506eef40310305ce3fa1759fe68ddfc7171d8
Jasper Barcial <Jbarcial25@gmail.com>
2022-02-05 12:53:00 -0800

    set limit for data to 10. also added 5 more box for the values

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
-// var dateTimeId = document.getElementsByClassName('#date-time');
-// var locationId = document.getElementById('.location');
-// var magnitudeId = document.getElementById('.magnitude');
-// var earthquakeHere = document.getElementById('earthQuake-here')
-
-var buttonx = document.getElementById('button-sample')
-
 function getInfo(){
     //API filters: Earthquake, start time, end time, and limit
-    var requestUrl = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&&minlatitude=32.5121&maxlatitude=42.0126&minlongitude=-124.6509&maxlongitude=-114.1315&starttime=2020-01-01&orderby=magnitude&eventtype=earthquake&endtime&limit=5'
+    var requestUrl = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&&minlatitude=32.5121&maxlatitude=42.0126&minlongitude=-124.6509&maxlongitude=-114.1315&starttime=2020-01-01&orderby=magnitude&eventtype=earthquake&endtime&limit=10'
     fetch(requestUrl)
         .then(function (response){
             return response.json();
@@ -45,8 +38,21 @@ function getInfo(){
 
             }
 
-            // this loop is getting all the 'features' object giving us direct info and access on what specific 'key' we want (i.e place, magnitute, date, etc...)
+    });
+}
 
+
+function earthQuakeByTime(){
+    //API filters: Earthquake, start time, end time, and limit
+    var requestUrl = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&&minlatitude=32.5121&maxlatitude=42.0126&minlongitude=-124.6509&maxlongitude=-114.1315&starttime=2020-01-01&orderby=time&eventtype=earthquake&endtime&limit=10'
+    fetch(requestUrl)
+        .then(function (response){
+            return response.json();
+        })
+        .then(function (data){
+            console.log(data) // all the data on the API
+
+            // this loop is getting all the 'features' object giving us direct info and access on what specific 'key' we want (i.e place, magnitute, date, etc...)
             for (var i = 0; i < data.features.length; i++) {
                 features = data.features[i];
                 magnitude = features.properties.mag
@@ -67,3 +73,5 @@ function getInfo(){
 }
 
 getInfo();
+
+earthQuakeByTime();
