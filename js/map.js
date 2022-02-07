4d574d3ca5909b32526337f13f66b35cc9938092
mrjeoffrey <b.jeoffrey@gmail.com>
2022-02-06 21:38:50 -0800

    brought map.js code into script.js

--- js/map.js
+++ js/map.js
@@ -1,13 +1,3 @@
-// Initializes GoogleMaps API in Maps Section
-function initMap() {
-	var options = {
-		center: { lat: 36.5935, lng: -122.612 },
-		zoom: 6,
-		mapTypeId: "hybrid",
-	};
-	map = new google.maps.Map(document.getElementById("map"), options);
-}
-
 // Fix this code for geomapping the latest earthquakes and earthquake severity
 // function mapPreview() {
 // 	var options = {
--- js/script.js
+++ js/script.js
@@ -1,75 +1,100 @@
-function getInfo(){
-    //API filters: Earthquake, start time, end time, and limit
-    var requestUrl = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&&minlatitude=32.5121&maxlatitude=42.0126&minlongitude=-124.6509&maxlongitude=-114.1315&starttime=2020-01-01&orderby=magnitude&eventtype=earthquake&endtime&limit=10'
-    fetch(requestUrl)
-        .then(function (response){
-            return response.json();
-        })
-        .then(function (data){
-            console.log(data) // all the data on the API
-            
-        //    Takes API data and sorts by magnitude, largest to smallest
-            const sorted = data.features.sort(
-                (a, b) => b.properties.mag - a.properties.mag 
-                );
-                console.log(sorted);
-            
+function getInfo() {
+	//API filters: Earthquake, start time, end time, and limit
+	var requestUrl =
+		"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&&minlatitude=32.5121&maxlatitude=42.0126&minlongitude=-124.6509&maxlongitude=-114.1315&starttime=2020-01-01&orderby=magnitude&eventtype=earthquake&endtime&limit=10";
+	fetch(requestUrl)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data); // all the data on the API
 
-             var features = data.features // the 'feature' object inside the 'data'
-            
-            console.log(features[1])
+			//    Takes API data and sorts by magnitude, largest to smallest
+			const sorted = data.features.sort(
+				(a, b) => b.properties.mag - a.properties.mag
+			);
+			console.log(sorted);
 
-            // This loop is rendering the date, location, and magnitude dynamically to the page
-            for (var i = 0; i < data.features.length; i++){
-                features = data.features[i];
-                var magnitude = features.properties.mag
-                var place = features.properties.place
-                var time = features.properties.time
+			var features = data.features; // the 'feature' object inside the 'data'
 
-                // These will list the recent earthquakes in sorted order of magnitude, largest to smallest
-                var day = document.getElementById(`date${i+1}`);
-                var loc = document.getElementById(`location${i+1}`);
-                var mag = document.getElementById(`mag${i+1}`);
+			console.log(features[1]);
 
-                myDate = new Date (time)
-                loc.textContent = place;
-                day.textContent = myDate;
-                mag.textContent = magnitude;
+			// This loop is rendering the date, location, and magnitude dynamically to the page
+			for (var i = 0; i < data.features.length; i++) {
+				features = data.features[i];
+				var magnitude = features.properties.mag;
+				var place = features.properties.place;
+				var time = features.properties.time;
 
-            }
+				// These will list the recent earthquakes in sorted order of magnitude, largest to smallest
+				var day = document.getElementById(`date${i + 1}`);
+				var loc = document.getElementById(`location${i + 1}`);
+				var mag = document.getElementById(`mag${i + 1}`);
 
-    });
+				myDate = new Date(time);
+				loc.textContent = place;
+				day.textContent = myDate;
+				mag.textContent = magnitude;
+			}
+		});
 }
 
+function earthQuakeByTime() {
+	//API filters: Earthquake, start time, end time, and limit
+	var requestUrl =
+		"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&&minlatitude=32.5121&maxlatitude=42.0126&minlongitude=-124.6509&maxlongitude=-114.1315&starttime=2020-01-01&orderby=time&eventtype=earthquake&endtime&limit=10";
+	fetch(requestUrl)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data); // all the data on the API
 
-function earthQuakeByTime(){
-    //API filters: Earthquake, start time, end time, and limit
-    var requestUrl = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&&minlatitude=32.5121&maxlatitude=42.0126&minlongitude=-124.6509&maxlongitude=-114.1315&starttime=2020-01-01&orderby=time&eventtype=earthquake&endtime&limit=10'
-    fetch(requestUrl)
-        .then(function (response){
-            return response.json();
-        })
-        .then(function (data){
-            console.log(data) // all the data on the API
+			// this loop is getting all the 'features' object giving us direct info and access on what specific 'key' we want (i.e place, magnitute, date, etc...)
+			for (var i = 0; i < data.features.length; i++) {
+				features = data.features[i];
+				magnitude = features.properties.mag;
+				place = features.properties.place;
+				time = features.properties.time;
+				// console.log(features.properties.mag);
+				myDate = new Date(time);
 
-            // this loop is getting all the 'features' object giving us direct info and access on what specific 'key' we want (i.e place, magnitute, date, etc...)
-            for (var i = 0; i < data.features.length; i++) {
-                features = data.features[i];
-                magnitude = features.properties.mag
-                place = features.properties.place
-                time = features.properties.time
-                // console.log(features.properties.mag);
-                myDate = new Date(time)
+				var locationSev = document.getElementById(`sever-location${i + 1}`);
+				var magSev = document.getElementById(`sever-mag${i + 1}`);
+				var dateSev = document.getElementById(`sever-date${i + 1}`);
 
-                var locationSev = document.getElementById(`sever-location${i+1}`)
-                var magSev = document.getElementById(`sever-mag${i+1}`)
-                var dateSev = document.getElementById(`sever-date${i+1}`)
+				locationSev.innerText = place;
+				magSev.innerText = magnitude;
+				dateSev.innerText = myDate;
 
-                locationSev.innerText = place
-                magSev.innerText = magnitude
-                dateSev.innerText = myDate
-            }
-    });
+				console.log(magnitude, place, time);
+			}
+		});
+}
+
+// need a couple VARS to translate data from city into long / lat
+// use split method at marker 10 and do a .length to grab the city
+// use .trim
+
+/*
+https://maps.googleapis.com/maps/api/staticmap?center=" + 
+inputSpecWithPlus + "," + inputCityWithPlus + "," + inputStateAbbv
+Brooklyn+Bridge,New+York,NY 
+"&zoom=13&size=90x62&maptype=satellite
+&markers=color:red%7Clabel:S%7C" +
+inputLong + "," + inputLat + 
+40.702147,-74.015794
+"&key=YOUR_API_KEY"
+*/
+
+// Initializes GoogleMaps API in Maps Section
+function initMap() {
+	var options = {
+		center: { lat: 36.5935, lng: -122.612 },
+		zoom: 6,
+		mapTypeId: "hybrid",
+	};
+	map = new google.maps.Map(document.getElementById("map"), options);
 }
 
 getInfo();
