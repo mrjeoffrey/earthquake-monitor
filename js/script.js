7c1b1dad2fd79dbc2c312583f61afcb2a4974c24
aldreanelafont <aldreanelafuente@yahoo.com>
2022-02-07 22:00:11 -0800

    Finalizing changes

--- js/script.js
+++ js/script.js
@@ -1,104 +1,54 @@
-// var submitButtonEl = document.getElementById("submit-btn")
-// submitButtonEl.addEventListener ("click", getInfo)
-var searchInput = document.querySelector("search-input")
-let requestUrl = []
-//var longitude = data.features[i].geometry.coordinates[0]
-//var latitude = data.features[i].geometry.coordinates[1]
-
-let zipCode = []
-//var currentLoc = "https://ipapi.co/json"
-
-
-// function getInput() {
-//     var 
-
-// function getValue() {
-//     var searchInput
+// var searchInput = document.getElementById("input")
+// console.log (searchInput)
+// var submitButtonEl = document.querySelector("submit-btn")
+// function getValue(){
+//     var inputVal = document.querySelector("input")
+//     var value = inputVal.value
+//     console.log(value)
 // }
+// getValue();
 
-
-function changeLoc(){
-    var searchInput1 = searchInput.value
-    console.log(searchInput1)
-    var zipUrl = "https://nominatim.openstreetmap.org/search?postalcode=" + searchInput1 + "&country=USA&format=json"
-    fetch(zipUrl).then(function (response) {
-        return response.json()
-    }).then(function (data){
-        latitude = data[0].latitude
-        longitude = data[0].longitude
-        //city = data[0].display_name.substring(0, data[0].display_name.indexOf(","))
-        clear()
-    }).catch(function () {
-    //     zipCode = "Search here"
-    //     searchInput.value = ""
-    //     searchInput.placeholder = zipCode
+function geoCode(){
     
-    }
-    )
-var submitButtonEl = document.getElementById("submit-btn")
+    // var APIKey = "AIzaSyAv_sX-T1PMgiRLVrTjhrRAbJ-K7Plw9Nw"
+    // var longitude = "-119.4179324"
+    // var latitude = "36.778261"
+    var url = "https://maps.googleapis.com/maps/api/geocode/json?address=California?postal_code?&key=AIzaSyAv_sX-T1PMgiRLVrTjhrRAbJ-K7Plw9Nw"
+    console.log(url);
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)  
 
-submitButtonEl.addEventListener('click', getValue)
-
-function getValue(){
-    var inputVal = document.querySelector("input")
-    var value = inputVal.value
-
-    console.log(value)
-}
-// function changeVal() {
-//     const longlat = ol.proj.transform(map.getView().getCenter(), 'EPSG:3857', 'EPSG:4326')
-//     latitude = longlat[1]
-//     longiitude = longlat[0]
-//     var localValue = "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=" + latitude + "&lon=" + longitude + "&zoom="
-//     fetch(localValue).then(function (response){
-//         return response.json()
-//     }).then(function (data) {
-//         city = data.display_name.substring(data.display_name.indexOf(",") + 1)
-//         city = city.substring(0, city.indexOf(","))
-//         var matchData = data.display_name.match(/\b\d{5}\b/g)
-
-//         if (matchData) {
-//             zipCode = matchData[0]
-//         } else {
-//             zipCode = "00000"
-//         }
-//         searchInput.value = zipCode
-//         clear()
-
-//     })
-
-//     console.log(changeVal)
-// }
-
-
-//inputVal.addEventListener("input", e => {
-    //var value = e.target.value
-    //console.log(value)
-//})
-/*let data = []
-submitButtonEl.addEventListener('click', getInfo)
-
-inputVal.addEventListener("input", e => {
-    const value = e.target.value
-    data.forEach(features => {
-        const isVisible = features.properties.place.includes(value) 
-
-    })
-})*/
-
-
-//var searchVal = document.getElementbyId ("search-input")
-    //inputVal.toLowerCase();
+        })
     
-//function search_bar(){
     
-    //var inputVal = document.getInfo()
+        geoCode();
 
-/*function getInfo(){
-    var inputVal = document.querySelector("input")
-    var value = inputVal.value
-    console.log(inputVal)
-}*/
+
+
+function searchCoordinates(longitude, latitude){
+    var requestUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&&latitude=%22+latitude+%22&longitude=%22+longitude+%22&maxradiuskm=300&orderby=time&eventtype=earthquake&endtime&limit=5"
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data) 
+
+            for (var i = 0; i < data.features.length; i++) {
+                features = data.features[i];
+                magnitude = features.properties.mag
+                place = features.properties.place
+                time = features.properties.time
+
+                myDate = new Date(time)
+            
+            }
+
+    });
 
 function getInfo(){
     //API filters: Earthquake, start time, end time, and limit
@@ -161,6 +111,9 @@ function earthQuakeByTime(){
         .then(function (data){
             console.log(data) // all the data on the API
 
+
+            // var longitude = data.features[i].geometry.coordinates[0]
+            // var latitude = data.features[i].geometry.coordinates[1]
             // this loop is getting all the 'features' object giving us direct info and access on what specific 'key' we want (i.e place, magnitute, date, etc...)
             for (var i = 0; i < data.features.length; i++) {
                 features = data.features[i];
@@ -183,4 +136,4 @@ function earthQuakeByTime(){
 
 getInfo();
     
-earthQuakeByTime();}
+earthQuakeByTime();}}
--- js/search-bar.js
+++ js/search-bar.js
@@ -86,4 +86,105 @@ function getInfo(){
 getInfo();
 */
 
-          
+ // var submitButtonEl = document.getElementById("submit-btn")
+// submitButtonEl.addEventListener ("click", getInfo)
+var searchInput = document.querySelector("search-input")
+let requestUrl = []
+//var longitude = data.features[i].geometry.coordinates[0]
+//var latitude = data.features[i].geometry.coordinates[1]
+let latitude = []
+let longitude = []
+let zipCode = []
+//var currentLoc = "https://ipapi.co/json"
+
+
+// function getInput() {
+//     var 
+
+// function getValue() {
+//     var searchInput
+// }
+
+
+function changeLoc(){
+    var searchInput1 = searchInput.value
+    console.log(searchInput1)
+    var zipUrl = "https://nominatim.openstreetmap.org/search?postalcode=" + searchInput1 + "&country=USA&format=json"
+    fetch(zipUrl).then(function (response) {
+        return response.json()
+    }).then(function (data){
+        latitude = data[1].latitude
+        longitude = data[0].longitude
+        //city = data[0].display_name.substring(0, data[0].display_name.indexOf(","))
+        clear()
+    }).catch(function () {
+    //     zipCode = "Search here"
+    //     searchInput.value = ""
+    //     searchInput.placeholder = zipCode
+    
+    }
+    )
+var submitButtonEl = document.getElementById("submit-btn")
+
+submitButtonEl.addEventListener('click', getValue)
+
+function getValue(){
+    var inputVal = document.querySelector("input")
+    var value = inputVal.value
+
+    console.log(value)
+}
+// function changeVal() {
+//     const longlat = ol.proj.transform(map.getView().getCenter(), 'EPSG:3857', 'EPSG:4326')
+//     latitude = longlat[1]
+//     longiitude = longlat[0]
+//     var localValue = "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=" + latitude + "&lon=" + longitude + "&zoom="
+//     fetch(localValue).then(function (response){
+//         return response.json()
+//     }).then(function (data) {
+//         city = data.display_name.substring(data.display_name.indexOf(",") + 1)
+//         city = city.substring(0, city.indexOf(","))
+//         var matchData = data.display_name.match(/\b\d{5}\b/g)
+
+//         if (matchData) {
+//             zipCode = matchData[0]
+//         } else {
+//             zipCode = "00000"
+//         }
+//         searchInput.value = zipCode
+//         clear()
+
+//     })
+
+//     console.log(changeVal)
+// }
+
+
+//inputVal.addEventListener("input", e => {
+    //var value = e.target.value
+    //console.log(value)
+//})
+/*let data = []
+submitButtonEl.addEventListener('click', getInfo)
+
+inputVal.addEventListener("input", e => {
+    const value = e.target.value
+    data.forEach(features => {
+        const isVisible = features.properties.place.includes(value) 
+
+    })
+})*/
+
+
+//var searchVal = document.getElementbyId ("search-input")
+    //inputVal.toLowerCase();
+    
+//function search_bar(){
+    
+    //var inputVal = document.getInfo()
+
+/*function getInfo(){
+    var inputVal = document.querySelector("input")
+    var value = inputVal.value
+    console.log(inputVal)
+}*/     }   
