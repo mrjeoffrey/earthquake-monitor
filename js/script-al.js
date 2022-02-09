c5492967e0eaea299f5f582016d390899b705a18
jessibermudez <jessmag02@gmail.com>
2022-02-09 00:25:21 -0800

    added widget map code to script-al.js, added favicon to html, moved bigMap code to script-al.js, commented my code, deleted script-jessi.js, commented out script.js bc all code is on script-al.js (waiting on team approval to delete)

--- js/script-al.js
+++ js/script-al.js
@@ -105,16 +105,29 @@ function getInfo() {
 				var magnitude = features.properties.mag;
 				var place = features.properties.place;
 				var time = features.properties.time;
+				// These variables pull the coordinates individually from a geometry point 
+				var lat = features.geometry.coordinates[1];
+				var long = features.geometry.coordinates[0];
+				// This variable loops through the coordinates-however this does not function as coord on its own
+				var coord = lat,long
+					console.log(lat,long)
 
 				// These will list the recent earthquakes in sorted order of magnitude, largest to smallest
 				var day = document.getElementById(`date${i + 1}`);
 				var loc = document.getElementById(`location${i + 1}`);
 				var mag = document.getElementById(`mag${i + 1}`);
+				// This variable pulls all ids in order 
+				var displayLittleMap = document.getElementById(`severeMap${i + 1}`); 
 
 				myDate = new Date(time);
 				loc.textContent = place;
 				day.textContent = myDate;
 				mag.textContent = magnitude + " M";
+				// The elements are called and the API url for the static map is added. 
+				// The variables set for the coordinates being pulled are added here. This
+				// in turn produces maps that are constantly adapting to the newly pulled coordinates
+				displayLittleMap.src = `https://maps.googleapis.com/maps/api/staticmap?&center=${lat},${long}&zoom=9&size=90x62&scale=2&maptype=hybrid&markers=size:tiny|color:red|${lat},${long}&key=AIzaSyC72d4HR-Hqfy8tUtGo4nJDGGvE4ux2Brw`;
+
 			}
 		});
 }
@@ -133,18 +146,32 @@ function earthQuakeByTime() {
 			for (var i = 0; i < data.features.length; i++) {
 				features = data.features[i];
 				magnitude = features.properties.mag;
+				// console.log(features.properties.mag);
 				place = features.properties.place;
 				time = features.properties.time;
-				// console.log(features.properties.mag);
+				// These variables pull the coordinates individually from a geometry point 
+				lat = features.geometry.coordinates[1];
+				long = features.geometry.coordinates[0];
+				// This variable loops through the coordinates-however this does not function as coord on its own
+				coord = lat,long
+				console.log(lat,long)
+				
 				myDate = new Date(time);
 
 				var locationSev = document.getElementById(`sever-location${i + 1}`);
 				var magSev = document.getElementById(`sever-mag${i + 1}`);
 				var dateSev = document.getElementById(`sever-date${i + 1}`);
+				// This variable pulls all ids in order 
+				var displayLittleMap = document.getElementById(`latestMap${i + 1}`);
 
 				locationSev.innerText = place;
 				magSev.innerText = magnitude + " M";
 				dateSev.innerText = myDate;
+				// The elements are called and the API url for the static map is added. 
+				// The variables set for the coordinates being pulled are added here. This
+				// in turn produces maps that are constantly adapting to the newly pulled coordinates
+				displayLittleMap.src = `https://maps.googleapis.com/maps/api/staticmap?&center=${lat},${long}&zoom=9&size=90x62&scale=2&maptype=hybrid&markers=size:tiny|color:yellow|${lat},${long}&key=AIzaSyC72d4HR-Hqfy8tUtGo4nJDGGvE4ux2Brw`;
+
 			}
 		});
 }
@@ -186,6 +213,48 @@ historyEl.addEventListener("click", function (event) {
 	userValue = valueCity;
 	geoCode();
 });
+
+// Sets bigMap as a global variable
+let bigMap;
+// this function is called when the google api library is downloaded using the mapDisplay callback 
+function mapDisplay() {
+	// combines all map preferences under one variab;e
+	var options = {
+		center: { lat: 36.5935, lng: -122.612 },
+		zoom: 6,
+		mapTypeId: "hybrid",
+	};
+	// grabs map id to display the map
+	bigMap = new google.maps.Map(document.getElementById("map"), options);
+	// dynamically creates a script tag
+	var latestUsgs = document.createElement("script");
+	// assigns the google API url to that script tag, then appends the script to head
+	latestUsgs.src =
+		"https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
+	document.getElementsByTagName("head")[0].appendChild(latestUsgs);
+	// pulls magnitude property and sets a marker
+	bigMap.data.setStyle((feature) => {
+		const magnitude = feature.getProperty("mag");
+		return {
+			icon: getCircle(magnitude),
+		};
+	});
+}
+// sets the styling to the marker pinned above
+function getCircle(magnitude) {
+	return {
+		path: google.maps.SymbolPath.CIRCLE,
+		fillColor: "orange",
+		fillOpacity: 0.35,
+		scale: Math.pow(3, magnitude) / 2,
+		strokeColor: "white",
+		strokeWeight: 0.8,
+	};
+}
+// this calls the API data from google map API and adds the geojson layer to the map
+function eqfeed_callback(results) {
+	bigMap.data.addGeoJson(results);
+}
 
 getInfo();
 earthQuakeByTime();
